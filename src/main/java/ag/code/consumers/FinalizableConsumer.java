5f948585848f65074896ba7b588da980e02832e7
André Garcia <andre.garcia85@gmail.com>
2022-01-22 22:42:56 +0000

    fix generics

--- src/main/java/ag/code/consumers/FinalizableConsumer.java
+++ src/main/java/ag/code/consumers/FinalizableConsumer.java
@@ -2,7 +2,7 @@ package ag.code.consumers;
 
 import java.util.function.Consumer;
 
-public interface FinalizableConsumer extends Consumer {
+public interface FinalizableConsumer<T> extends Consumer<T> {
 
     default void finalize(){ };
 
