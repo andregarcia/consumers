6f17e8473d7fe2678602a4ca29735ae4dd83a782
André Garcia <andre.garcia85@gmail.com>
2022-01-22 23:54:10 +0000

    add join method

--- src/main/java/ag/code/consumers/AsyncConsumers.java
+++ src/main/java/ag/code/consumers/AsyncConsumers.java
@@ -1,10 +1,7 @@
 package ag.code.consumers;
 
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.function.Consumer;
 
 
@@ -14,8 +11,13 @@ public class AsyncConsumers<T> {
 
     private ThreadPoolExecutor threadPoolExecutor;
 
+    private List<Future> tasks;
+
+    private Consumer<Exception> exceptionHandler;
+
     public AsyncConsumers(int nThreads) {
         consumers = new LinkedList<>();
+        tasks = Collections.synchronizedList(new ArrayList<>());
         threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(nThreads);
     }
 
@@ -25,11 +27,26 @@ public class AsyncConsumers<T> {
     }
 
     public void consume(T t) {
-        consumers.forEach((consumer) -> threadPoolExecutor.submit(() -> consumer.accept(t)));
+        consumers.forEach((consumer) -> {
+            Future<?> task = threadPoolExecutor.submit(() -> consumer.accept(t));
+            tasks.add(task);
+        });
     }
 
     public void consume(Collection<T> collection) {
-        collection.forEach(t -> consume(t));
+        collection.forEach(this::consume);
+    }
+
+    public void join(long timeout, TimeUnit timeUnit) throws ExecutionException,
+            InterruptedException, TimeoutException {
+        for (Future<?> f : tasks) {
+            try {
+                f.get(timeout, timeUnit);
+            } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                if (exceptionHandler != null) exceptionHandler.accept(e);
+                else throw e;
+            }
+        }
     }
 
     public void finalize() {
